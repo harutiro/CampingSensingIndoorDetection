dfd992e2802ebaea0e39bd2439359ca0e5a1c6b4
harutiro <harutiro2027@outlook.jp>
2023-04-03 23:16:40 +0900

    Initial commit

---
+++ app/src/main/java/net/harutiro/campingsensingindoordetection/MainActivity.kt
@@ -0,0 +1,11 @@
+package net.harutiro.campingsensingindoordetection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
